98d9548bfb516615c0de5d4be9351145724bf80d
seungeon <seungeonjin23@gmail.com>
2024-08-11 12:31:19 +0900

    set2  #1100

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
     # 데이터 로드
     start_idx = 1000
     base_path = 'E:\set2'
+    # 컴퓨팅 자원 관계상 idx를 바꿔가며 1000개의 파일씩 학습을 진행
+    
     train_data = load_data_from_directory(base_path, 'training',start_idx) # ('car', PointCloud with 1323 points.)
     val_data = load_data_from_directory(base_path, 'validation', start_idx)
-    
-    # 컴퓨팅 자원 관계상 idx를 바꿔가며 1000개의 파일씩 학습을 진행
     test_data = load_data_from_directory(base_path, 'test', start_idx)
 
     print(f'Train data: {len(train_data)}')
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     # 모델 로드
     model_path = 'pointnet_model.pth'
     if os.path.exists(model_path):
-        pointnet.load_state_dict(torch.load(model_path))
+        pointnet.load_state_dict(torch.load(model_path, weights_only=True))
         print(f'Model loaded from {model_path}')
     else:
         print(f'No model found at {model_path}, starting from scratch.')
@@ -48,4 +48,4 @@ if __name__ == "__main__":
     train_model(pointnet, train_loader, val_loader)
     # 테스트 데이터셋으로 평가
     num_classes = 9 # 0~8 0은 배경 클래스라고 가정
-    test_model(pointnet, test_loader, num_classes)
+    # test_model(pointnet, test_loader, num_classes)
